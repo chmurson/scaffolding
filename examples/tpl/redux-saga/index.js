ed8eea4f5c9d754035c67de686a06be819296274
Wojciech Piątkowski <chmurson@gmail.com>
2018-11-04 17:48:44 +0100

    clear gh-pages branch

--- examples/tpl/redux-saga/index.js
+++
@@ -1,3 +0,0 @@
-export { default as {{name}}Container } from './{{name}}Container';
-export { selectors, moduleName } from './selectors';
-export { actions, saga, reducer } from './duck';
--- examples/tpl/redux-saga/selectors.js
+++
@@ -1,7 +0,0 @@
-export const moduleName = "{{ name }}";
-
-const localState = state => state[moduleName];
-
-export const selectors = {
-    example: state => localState(state).exampleValue,
-};
